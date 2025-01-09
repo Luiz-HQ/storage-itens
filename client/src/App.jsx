06816ce051c0e3097ad6685d326ab45b8671311e
luizdev <luizhn1703@gmail.com>
2025-01-09 01:17:52 -0300

    start front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-      <h1>Cadastro de produtos</h1>
+      <Home />
     </>
   );
 }
---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <h1>Cadastro de produtos</h1>
+    </>
+  );
+}
+
+export default Home;
